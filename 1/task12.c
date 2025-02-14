1d701b55718701c7a618d37436c0b96d2b87fb72
Ruslan Safaroff <62283143+NimblePython@users.noreply.github.com>
2025-02-14 21:40:04 +0300

    Логическая операция &&
    Одно &. - это побитовая операция И. 
    А нас интересует сейчас логическая -  &&

--- 1/task12.c
+++ 1/task12.c
@@ -4,7 +4,7 @@ int main() {
     int a;
     printf("Type angle of ship: ");
     scanf("%d", &a);
-    if (a <= 180 & a >= -180) {
+    if (a <= 180 && a >= -180) {
         printf("OK\n");
     } else {
         printf("NOT OK\n");
