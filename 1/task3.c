a01942c88ce37f4efdb818ca68abc5c44d000b01
Ruslan Safaroff <62283143+NimblePython@users.noreply.github.com>
2025-02-14 21:41:03 +0300

    Так же про &&

--- 1/task3.c
+++ 1/task3.c
@@ -4,7 +4,7 @@ int main() {
     char s;
     printf("Type engine mark: ");
     scanf("%c", &s);
-    if (s >= 'A' & s <= 'Z') {
+    if (s >= 'A' && s <= 'Z') {
         printf("OK\n");
     } else {
         printf("NOT OK\n");
